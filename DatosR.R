291edfc6ebfc1d0443adfa2a1e43e5079090d3ee
javieramedina <javiera.medina.j@gmail.com>
2019-03-21 00:13:41 -0300

    Comandos para variables dummies, se utiliza el excel "datos", hoja de calculo "datos para R"

--- DatosR.R
+++ DatosR.R
@@ -1,2 +1,13 @@
-modelo <- lm(Compra~Busquedas+Retroalimentacion+Tipocompra, data = DATOS)
-summary(modelo)
+#Se crean variables dummies
+#se reconoce como modelo base; sexo:femenino;retro:bueno;conocimiento:bueno
+sexo<-factor(datos$sexo)
+is.factor(sexo)
+conocimiento<-factor(datos$conocimiento)
+is.factor(conocimiento)
+retro<-factor(datos$retro)
+is.factor(retro)
+
+###########Generar modelo full######################
+
+modelofull<-lm(datos$compra~datos$edad++sexo+datos$motor+conocimiento+datos$busquedas+retro+datos$fiamarca+datos$tipocompra+datos$estructuraweb)
+summary(modelofull)
