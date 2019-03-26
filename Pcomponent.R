2cfe2d05b2bf7134af8b158e4639737ff69790d0
javieramedina <javiera.medina.j@gmail.com>
2019-03-25 23:27:56 -0300

    comandos

--- Pcomponent.R
+++ Pcomponent.R
@@ -1,32 +1,54 @@
-##Correlacion de variables##
-cor(datos)
+## Principal Component
+## Para reconocer la media de la matriz de datos
+promedioapc<-apply(X = datosfinal, MARGIN = 2, FUN = mean)
+## Para reconocer la varianza de datos 
+varianzaapc<-apply(X = datosfinal, MARGIN = 2, FUN = var)
 
-round(cor(datos), 3)
+## Por defecto, la funcion "prcomp" centra las variables para que tengan media cero, pero si además se quiere que la desviacion estandar
+## sea de 1, hay que indicar    scale = TRUE
+datosapc<-prcomp(datosfinal, scale=TRUE)
+## Entrega la media de los datos previos a estandarizacion
+datosapc$center
+# Entrega la desviación estándar de los datos previos a estandarizacion
+datosapc$scale
+## Entrega el valor de los loadings en base a (n-1,p) en este caso, matriz de (546,7)
+datosapc$rotation
+## Entrega el resultado de cada componente principal para cada individuo de la muestra
+datosapc$x
+## Entrega los valores de los 6 primeros individuos
+head(datosapc$x)
+## Entrega las dimensiones de la matriz datos.apc
+dim(datosapc$x)
+## Genera un grafico bidimensional de las dos primeras componentes, se establece el argumento scale=0 para que las flechas estén en la misma escala que las componentes
+biplot(x = datosapc, scale = 0, cex = 0.6, col = c("blue4", "brown3"))
+## La imagen especular, cuya interpretacion es equivalente, se obtiene invirtiendo el signo de los loadings y los principal component scores
+datosapc$rotation <- -datosapc$rotation
+datosapc$x <- -datosapc$x
+biplot(x = datosapc, scale = 0, cex = 0.6, col = c("blue4", "brown3"))
+## Se comienza a observar el comportamiento de las varianzas de los datos,apc
+library(ggplot2)
+datosapc$sdev^2
+## Se saca la proporcion de la varianza 
+propvarianza<-datosapc$sdev^2 / sum(datosapc$sdev^2)
+propvarianza
+## Graficar la proporcion de la varianza explicada vs las componentes principales
+ggplot(data = data.frame(propvarianza, pc = 1:7), aes(x = pc, y = propvarianza)) + geom_col(width = 0.5) + scale_y_continuous(limits = c(0,0.3)) + theme_bw() + labs(x = "Componente principal", y = "Prop. de varianza explicada")
+## Calcular la proporcion de la varianza explicada acumulada
+propvarianzaacum<-cumsum(propvarianza)
+propvarianzaacum
+## Graficar la proporcion de la varianza explicada acumulada vs las componentes principales
+ggplot(data = data.frame(propvarianzaacum, pc = 1:7), aes(x = pc, y = propvarianzaacum, group = 1)) +geom_point() + geom_line() + theme_bw() + labs(x = "Componente principal",y = "Prop. varianza explicada acumulada")
 
-guardaResultados <- cor(datos)
-guardaResultados <- round(cor(datos))
+promedioapc<-apply(X = datosfinal, MARGIN = 2, FUN = mean)
+## Para reconocer la varianza de datos 
+varianzaapc<-apply(X = datosfinal, MARGIN = 2, FUN = var)
 
-datos.pca<-prcomp(datos, scale=TRUE)
-guardaResultados <- datos.pca
-
-summary(datos.pca)
-media<-datos.pca$center
-desvstandar<-datos.pca$scale
-
-cor(compra, sexo)
-
-sexo<-datos$sexo
-compra<-datos$compra
-cor(compra, sexo)
-
-
-plot(compra, sexo)
-plot(sexo, compra)
-
-barplot(compra, sexo)
-
-round(cor(datos), 4)
-
-
-
+## Por defecto, la funcion "prcomp" centra las variables para que tengan media cero, pero si además se quiere que la desviacion estandar
+## sea de 1, hay que indicar    scale = TRUE
+datosapc<-prcomp(datosfinal, scale=TRUE)
+## Entrega la media de los datos previos a estandarizacion
+datosapc$center
+# Entrega la desviación estándar de los datos previos a estandarizacion
+datosapc$scale
+## Entrega el valor de los loadings en base a (n-1,p) en este caso, matriz de (546,7)
 
--- Regresion.R
+++ Regresion.R
@@ -1,51 +1,87 @@
-#Se crean variables dummies
+compra<-datos$compra
+edad<-datos$edad
+sexo<-datos$sexo
+comuna<-datos$comuna
+buscador<-datos$buscador
+cono1<-datos$cono1
+cono2<-datos$cono2
+cono3<-datos$cono3
+busqueda<-datos$busqueda
+retro1<-datos$retro1
+retro2<-datos$retro2
+retro3<-datos$retro3
+retro4<-datos$retro4
+fiamarc<-datos$fiamarc
+tipocompra<-datos$tipocompra
+estructuraweb<-datos$estructuraweb
+
+rm(sexo)
 sexo<-factor(datos$sexo)
 is.factor(sexo)
-comuna <-factor(datos$comuna)
-is.factor(comuna)
-
-##MODELO CON TODAS LAS VARIBALES###
-modelofull<-lm(datos$compra~datos$edad+sexo+datos$comuna+datos$buscador+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$cono3+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-modelofull<-lm(datos$compra~datos$edad+datos$sexo+datos$comuna+datos$buscador+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$cono3+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE EDAD##
-modelofull<-lm(datos$compra~datos$sexo+datos$comuna+datos$buscador+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$cono3+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE COMUNA##
-modelofull<-lm(datos$compra~datos$sexo+datos$buscador+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$cono3+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIBLE BUSCADOR##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$cono3+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE CONO3##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$fiamarc+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE FIAMARCA##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$retro2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE RETRO2##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$retro3+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE RETRO3##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$retro4+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE RETRO4##
-modelofull<-lm(datos$compra~datos$sexo+datos$retro1+datos$busqueda+datos$cono1+datos$cono2+datos$tipocompra+datos$estructuraweb)
-summary(modelofull)
-
-##SACANDO VARIABLE ESTRUCTURAWEB##
-modelofull<-lm(datos$compra~datos$sexo+datos$busqueda+datos$cono1+datos$cono2+datos$tipocompra)
-summary(modelofull)
+rm(buscador)
+buscador<-factor(datos$buscador)
+is.factor(buscador)
 
 
+## Datos de correlacion
+round(cor(datos),4)
+cor(datos)
 
+
+## Regresion lineal
+modelocompleto<-lm(compra~edad+sexo+comuna+retro1+buscador+cono1+cono2+cono3+busqueda+retro2+retro3+retro4+fiamarc+tipocompra+estructuraweb)
+modelosincomuna<-lm(compra~edad+sexo+retro1+buscador+cono1+cono2+cono3+busqueda+retro2+retro3+retro4+fiamarc+tipocompra+estructuraweb)
+modelocompleto
+
+summary(modelocompleto)
+
+## Se entrega R cuadrado ajustado
+summary(modelocompleto)
+## Entre la matriz de correlacion 
+round(cor(datos), 4)
+##Residuos
+residuos<-rstandard(modelocompleto)
+par(mfrow=c(1,3))  ## Divide en 3 la parte de graficos
+hist(residuos)
+boxplot(residuos)
+## La varianza de los errores constante
+plot(fitted.values(modelocompleto),rstandard(modelocompleto), xlab = "Valores ajustados", ylab = "Residuos estandarizados")
+abline(h=0)
+## comprax<-which(colnames(datos)=="comprax")
+
+##modelo final##
+modelofinal<-lm(compra~edad+sexo+cono1+cono2+busqueda+tipocompra)
+modelofinal
+summary(modelofinal)
+
+
+datosfinal
+plot(datosfinal)
+
+cov(datosfinal)
+
+library(fmsb)
+VIF(modelofinal)
+vif
+library(car)
+VIF(modelofinal)
+vif(modelofinal)
+VIF(modelofinal)
+
+vif(sexo)
+VIF(sexo)
+
+Anova(modelofinal)
+
+hist(datosfinal)
+plot(datosfinal)
+cov(datosfinal)
+
+residuos<-resid(modelofinal)
+ks.test(residuos,"pnorm")
+
+qqnorm(residuos)
+
+durbinWatsonTest(modelofinal)
+
+influence.measures(modelofinal)
